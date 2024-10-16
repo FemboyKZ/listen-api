97d5228ffe3acbd8c277d5a5009b44688af866a6
Dots <dotvnl@gmail.com>
2024-10-16 15:57:54 +0300

    fix import, add warn for missing key, feature: auth header

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+import dotenv from "../dotenv";
+dotenv.config();
 
 const express = require("express");
 const app = express();
@@ -7,7 +8,16 @@ app.use(express.json());
 const key = process.env.KEY;
 const port = process.env.PORT || 5000;
 
+if (!key) {
+  throw new Error("Missing key");
+}
+
 app.post("/command", (req, res) => {
+  const authHeader = req.headers["authorization"];
+  if (authHeader !== key) {
+    return res.status(401).send("Unauthorized");
+  }
+
   const { command } = req.body;
   console.log(`Received command: ${command}`);
 
